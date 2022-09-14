37e3877fbfa4dd7864e70062d48c2fda2deefb76
yzhangcs <yzhang.cs@outlook.com>
2022-09-14 09:50:55 +0800

    Minor fix

--- crfsrl/model.py
+++ crfsrl/model.py
@@ -162,7 +162,7 @@ class CRFSemanticRoleLabelingModel(Model):
         s_edge = self.edge_attn(x, x)
         s_role = self.role_attn(x, x).permute(0, 2, 3, 1)
         role_mask = s_role.new_ones(self.args.n_roles).gt(0)
-        role_mask[[0, self.args.prd_index]] = 0
+        role_mask[[self.args.nul_index, self.args.prd_index]] = 0
         s_role[..., self.args.nul_index] = 0
         s_role[..., 0, role_mask] = MIN
         s_role[..., 1:, self.args.prd_index] = MIN
